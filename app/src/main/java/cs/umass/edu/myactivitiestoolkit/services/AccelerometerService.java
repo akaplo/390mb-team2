3e6f1996c6b90e540fbd7eab5fdb09bb188be745
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-09-25 15:50:50 -0400

    Fixed detecting steps using built-in Android algorithm.  It works and sends the data to the UI properly now.

--- app/src/main/java/cs/umass/edu/myactivitiestoolkit/services/AccelerometerService.java
+++ app/src/main/java/cs/umass/edu/myactivitiestoolkit/services/AccelerometerService.java
@@ -171,8 +171,8 @@ public class AccelerometerService extends SensorService implements SensorEventLi
         mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
         //TODO : (Assignment 1) Register your step detector. Register an OnStepListener to receive step events
         mStepSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-        mSensorManager.registerListener(stepDetector,mStepSensor,SensorManager.SENSOR_DELAY_NORMAL);
-        stepDetector.registerOnStepListener(stepListener);
+        mSensorManager.registerListener(this, mStepSensor,SensorManager.SENSOR_DELAY_NORMAL);
+       //    stepDetector.registerOnStepListener(stepListener);
     }
 
     /**
